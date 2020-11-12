1e1a741f79c183d00c4fc3fe42b35a46084b2a0e
Byron Mallett <byronated@gmail.com>
2020-11-12 17:23:11 +1300

    Working address input selection using WidgetInteractor

---
+++ Plugins/HandTracking/Source/HandTracking/Private/HandInteractionFunctions.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HandInteractionFunctions.h"
+
+//void UHandInteractionFunctions::ShowtVirtualKeyboard(bool bShow, int32 UserIndex, TSharedPtr<IVirtualKeyboardEntry> TextEntryWidget) const
+//{
+//	FSlateApplication::ShowVirtualKeyboard(bShow, UserIndex, TextEntryWidget);
+//}
