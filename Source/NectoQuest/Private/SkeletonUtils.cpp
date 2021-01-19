e2399565c619918b3cdb7a60a1deb863fbe46e3d
Byron Mallett <byronated@gmail.com>
2021-01-19 17:47:01 +1300

    Added physics hands to mannequin.
    Fixed engine stall in UPhysicalAnimationComponent by setting a.ParallelAnimEvaluation to 0.

---
+++ Source/NectoQuest/Private/SkeletonUtils.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SkeletonUtils.h"
+#include "Animation/Skeleton.h"
+#include "..\Public\SkeletonUtils.h"
+
+//TArray<int32> USkeletonUtils::GetChildBoneIndexes(USkeletalMesh* SkeletalMesh, int32 ParentBoneIndex)
+//{
+//	check(SkeletalMesh);
+//
+//	auto outbones = TArray<int32>();
+//	SkeletalMesh->Skeleton->GetChildBones(ParentBoneIndex, outbones);
+//	return outbones;
+//}
