8c9c69e6fad8c8f1ab8c79ec783e4b41db10ca92
Byron Mallett <byronated@gmail.com>
2021-04-25 15:59:37 +1200

    Updater using forked OpenXRSdk zip containing loader binaries

--- UpdateUnrealOpenXRSDK.py
+++ UpdateUnrealOpenXRSDK.py
@@ -5,19 +5,25 @@ import argparse
 import io
 
 # Source URLs
-openxrsdk_zip_url = 'https://codeload.github.com/KhronosGroup/OpenXR-SDK/zip/refs/heads/master'
+openxrsdk_zip_url = 'https://github.com/Mystfit/OpenXR-SDK/releases/download/release-1.0.15/OpenXRSDK-binaries.zip'
 oculusopenxrsdk_zip_url = 'https://securecdn.oculus.com/binaries/download/?id=4015382005167023'
 
 # OpenXR SDK path structures
-openxr_sdk_base_path = "Engine/Source/ThirdParty/OpenXR"
+openxr_sdk_base_path = "Engine"
 openxr_sdk_structure = {
-    "OpenXR-SDK-master/include/": "include/"
+    "ARM64/bin/openxr_loader.dll": "Binaries/ThirdParty/OpenXR/hololens/arm64/openxr_loader.dll",
+    "x64/bin/openxr_loader.dll":   "Binaries/ThirdParty/OpenXR/win64/openxr_loader.dll",
+    "Win32/bin/openxr_loader.dll": "Binaries/ThirdParty/OpenXR/win32/openxr_loader.dll",
+    "ARM64/lib/openxr_loader.lib": "Source/ThirdParty/OpenXR/loader/hololens/arm64/openxr_loader.lib",
+    "x64/lib/openxr_loader.lib":   "Source/ThirdParty/OpenXR/loader/win64/openxr_loader.lib",
+    "Win32/lib/openxr_loader.lib": "Source/ThirdParty/OpenXR/loader/win32/openxr_loader.lib",
+    "include/":  "Source/ThirdParty/OpenXR/"
 }
 
 # Oculus OpenXR SDK path structures
 openxr_oculus_base_path = "Engine/Source/ThirdParty/Oculus/OculusOpenXRLoader/OculusOpenXRLoader"
 openxr_oculus_sdk_structure = {
-    "OpenXR/Include/": "Include/",
+    "OpenXR/Include/": "/Include",
     "OpenXR/Libs/Android/arm64-v8a/Release/": "Lib/arm64-v8a/",
     "OpenXR/Libs/Android/armeabi-v7a/Release/": "Lib/armeabi-v7a/"
 }
@@ -30,24 +36,25 @@ def open_zip(url):
 
 def get_filename(zip_file, source_prefix):
     for zipinfo in zip_file.infolist():
-        if zipinfo.filename.startswith(source_prefix) and not os.path.isdir(zipinfo.filename):
+        if zipinfo.filename.startswith(source_prefix):
             yield zipinfo
 
 def extract_zip(zip_file, dest_dir, structure):
     for sourcepath, destpath in structure.items():
+        dest_is_folder = destpath[-1] == '/'
         abs_destpath = os.path.normpath(os.path.join(dest_dir, destpath))
-    
+        
         if not zipfile.Path(zip_file, at=sourcepath).exists():
             print("Could not find source file {0} in zip".format(sourcepath))
             return
 
         for src in get_filename(zip_file, sourcepath):
-            dst = os.path.join(abs_destpath, os.path.basename(src.filename))
-            if not os.path.exists(os.path.dirname(dst)):
+            if not src.filename[-1] == '/':
+                dst = os.path.normpath(os.path.join(abs_destpath, src.filename)) if dest_is_folder else abs_destpath
                 os.makedirs(os.path.dirname(dst), exist_ok=True)
-            if not os.path.isdir(dst):
+                
+                print("Extracting {0} to {1}".format(src.filename, dst))
                 with open(dst, 'wb') as f:
-                    print("Extracting {0} to {1}".format(src.filename, dst))
                     f.write(zip_file.read(src))
 
 
